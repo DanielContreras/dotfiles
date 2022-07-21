b97203da3db3faaadb8978214e7bdd77258c3583
DanielContreras <contrerass.daniel@gmail.com>
2022-07-20 23:53:19 -0600

    Update: Change the way that lspconfig shows errors to hover

--- .config/nvim/lua/lsp-config/language-servers.lua
+++ .config/nvim/lua/lsp-config/language-servers.lua
@@ -68,3 +68,12 @@ require("lspconfig")["rust_analyzer"].setup({
 		["rust-analyzer"] = {},
 	},
 })
+
+-- Remove virtual text errors
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+	virtual_text = true,
+	underline = true,
+	signs = true,
+})
+vim.cmd([[autocmd CursorHold * lua vim.diagnostic.open_float()]])
+vim.cmd([[autocmd CursorHoldI * silent! lua vim.lsp.buf.signature_help()]])
