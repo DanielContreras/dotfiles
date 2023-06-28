8c37e8cc5dc499d0b5b06ae8ffec9aa6f27ddd1d
Daniel Contreras <3289608+DanielContreras@users.noreply.github.com>
2023-06-27 22:17:26 -0600

    Delete .config/nvim directory
    Deleting the old version of nvim config. This version had some deprecated packages or configuration files.

--- .config/nvim/lua/colorschemes-config/nordfox.lua
+++
@@ -1,9 +0,0 @@
--- Provide a default color scheme in the event that our desired scheme encounters and error
-vim.cmd("colorscheme default")
-local colorscheme = "nordfox"
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-	vim.notify("colorscheme " .. colorscheme .. " not found!")
-	return
-end
