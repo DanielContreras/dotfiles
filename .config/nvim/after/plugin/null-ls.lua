4112123e1e9a8fcf2c792b1bd62e4fe4c6b5ac18
Daniel Contreras <3289608+DanielContreras@users.noreply.github.com>
2023-06-30 16:05:11 -0600

    Add feature to auto format on save
    Note that this doesn't work for clangd since clangd the language server handles its own formatting.

--- .config/nvim/after/plugin/null-ls.lua
+++ .config/nvim/after/plugin/null-ls.lua
@@ -1,3 +1,4 @@
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
   return
@@ -16,5 +17,19 @@ null_ls.setup({
     formatting.latexindent,
     formatting.prettier,
   },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({
+        group = augroup,
+        buffer = bufnr,
+      })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
 })
-
