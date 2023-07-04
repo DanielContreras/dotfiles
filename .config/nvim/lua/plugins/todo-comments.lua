a1a1b2a4b162cbc8d52e275c85f3c9d4f4d082db
Daniel Contreras <3289608+DanielContreras@users.noreply.github.com>
2023-07-04 09:46:33 +0000

    Enable lazy loading
    This commit enables lazy loading. The main issue with this form of
    lazy loading is that you cannot launch using 'nvim .' from the command
    line since 'NvimTree' is disabled by default. Also, if you were to open
    a file, say 'nvim example.lua' then you wouldn't load most plugins until
    you go into insert mode. Ideally we'd load plugins on file open.
    
    Additionally, this commit temporarily removes LSP support.

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -1,10 +1,12 @@
 return {
-  "folke/todo-comments.nvim",
-  event = "VeryLazy",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
+	"folke/todo-comments.nvim",
+	cmd = { "TodoLocList", "TodoQuickfix", "TodoTelescope", "TodoTrouble" },
+	event = "InsertEnter",
+	-- event = "VeryLazy",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
 }
